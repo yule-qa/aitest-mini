b5b5f31c65727566d89e7b97a399832e7e77211e
308667244@qq.com <308667244@qq.com>
2020-12-15 23:34:37 +0800

    测试任务部分代码 part2

--- src/main/java/com/hogwartsmini/demo/service/Impl/AiTestTaskServiceImpl.java
+++ src/main/java/com/hogwartsmini/demo/service/Impl/AiTestTaskServiceImpl.java
@@ -6,7 +6,10 @@ import com.hogwartsmini.demo.common.TokenDb;
 import com.hogwartsmini.demo.dao.*;
 import com.hogwartsmini.demo.dto.ResultDto;
 import com.hogwartsmini.demo.dto.TokenDto;
+import com.hogwartsmini.demo.dto.task.AddHogwartsTestTaskDto;
+import com.hogwartsmini.demo.dto.task.TestTaskDto;
 import com.hogwartsmini.demo.entity.HogwartsTestCase;
+import com.hogwartsmini.demo.entity.HogwartsTestJenkins;
 import com.hogwartsmini.demo.entity.HogwartsTestTask;
 import com.hogwartsmini.demo.service.AiTestTaskService;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
 
 
 @Service
@@ -37,14 +42,39 @@ public class AiTestTaskServiceImpl implements AiTestTaskService {
 
     /**
      * 添加jenkins
-     * @param hogwartsTestTask
+     * @param taskDto
      * @return
      */
     @Override
-    public ResultDto save(TokenDto tokenDto, HogwartsTestTask hogwartsTestTask) throws IOException, URISyntaxException {
+    public ResultDto save(TestTaskDto taskDto, Integer taskListId) throws IOException, URISyntaxException {
         //todo
-        return ResultDto.success("成功",hogwartsTestTask);
+        AddHogwartsTestTaskDto taskTest=taskDto.getTestTask();
+        List<Integer> caseIdList = taskDto.getCaseIdList();
+
+        //再次校验testTask,caseIdList 是否为空
+        Integer createUserId=taskTest.getCreateUserId();
+        Integer testJenkinsId=taskTest.getTestJenkinsId();
+
+        //校验testJenkinsId是否为空校验
+
+        if(Objects.isNull(testJenkinsId)){
+            return  ResultDto.fail("用户默认Jenkinsid为空");
+        }
+
+        //根据用户默认JenkinsId查询Jenkins信息并做非空
+        HogwartsTestJenkins queryHogwartsTestJenkins=new HogwartsTestJenkins();
+        queryHogwartsTestJenkins.setCreateUserId(createUserId);
+        queryHogwartsTestJenkins.setId(testJenkinsId);
+        HogwartsTestJenkins resultHogwartsTestJenkins=hogwartsTestJenkinsMapper.selectOne(queryHogwartsTestJenkins);
+        if (Objects.isNull(resultHogwartsTestJenkins)){
+            return ResultDto.fail("用户默认Jenkins信息为空");
+
+        //查询测试用例，如id为25,26,28
+        List<HogwartsTestCase> hogwartsTestCases=hogwartsTestCaseMapper.selectByIds("25,26,28");
+        }
+        return ResultDto.success("成功");
     }
+
 
     @Override
     public ResultDto<HogwartsTestTask> delete(Integer taskId, Integer createUserId) {
